8e635344ffc35795f5b27cb4d14ae52eabe25d14
Brainfogz <sinr0202@gmail.com>
2016-01-14 02:41:24 -0500

    fixed anchor glitch and detailed css

--- app/assets/javascripts/minch/app.js
+++ app/assets/javascripts/minch/app.js
@@ -1,32 +1,16 @@
 $(document).ready(function() {
 
-	$("#digitalFeedback").hide();
-	$("#IQCFeedback").hide();
-	$("#scarsinFeedback").hide();
+  $("#digitalOpen").click(function(){
+    $("#digitalFeedback").slideToggle("slow");
+  });
 
-	$("#digitalOpen").click(function(){
-        $("#digitalFeedback").slideToggle("slow");
-    });
+  $("#iqcOpen").click(function(){
+    $("#IQCFeedback").slideToggle("slow");
+  });
 
-    $("#iqcOpen").click(function(){
-        $("#IQCFeedback").slideToggle("slow");
-    });
-
-    $("#scarsinOpen").click(function(){
-        $("#scarsinFeedback").slideToggle("slow");
-    });
-
-      $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
+  $("#scarsinOpen").click(function(){
+    $("#scarsinFeedback").slideToggle("slow");
   });
 
 });
+
